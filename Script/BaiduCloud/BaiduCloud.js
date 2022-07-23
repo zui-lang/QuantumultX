2eb787fcd2e21c1043dd0e570579a74629f363e1
zui-lang <77880495+zui-lang@users.noreply.github.com>
2022-07-23 22:53:49 +0800

    Update BaiduCloud.js

--- Script/BaiduCloud/BaiduCloud.js
+++ Script/BaiduCloud/BaiduCloud.js
@@ -1,53 +1,13 @@
-/*
-作者： NobyDa
-原连接： https://github.com/NobyDa/Script/blob/master/Surge/JS/BaiduCloud.js
-百度网盘 解锁在线视频倍率/清晰度
-***************************
-QuantumultX:
-[rewrite_local]
-https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
-[mitm]
-hostname = pan.baidu.com
-***************************
-Surge4 or Loon:
-[Script]
-http-response https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/zui-lang/QuantumultX/main/Script/BaiduCloud/BaiduCloud.js
-hostname = pan.baidu.com
-**************************/
+// ==UserScript==
+// @ScriptName        百度网盘
+// @Author            @Cuttlefish
+// @WechatID          公众号墨鱼手记
+// @Function          倍速
+// @ScriptTime        20210705
+// @ScriptURL         https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Rewrite/UnlockVip/BaiduCloud.conf
+// ==/UserScript==
 
-if ($response.body) {
-    $done({
-        body: JSON.stringify({
-            "product_infos": [{
-                "product_id": "5310897792128633390",
-                "start_time": 1417260485,
-                "end_time": 2147483648,
-                "buy_time": "1417260485",
-                "cluster": "offlinedl",
-                "detail_cluster": "offlinedl",
-                "product_name": "gz_telecom_exp"
-            }, {
-                "product_name": "svip2_nd",
-                "product_description": "超级会员",
-                "function_num": 0,
-                "start_time": 1553702399,
-                "buy_description": "",
-                "buy_time": 0,
-                "product_id": "1",
-                "auto_upgrade_to_svip": 0,
-                "end_time": 1672502399,
-                "cluster": "vip",
-                "detail_cluster": "svip",
-                "status": 0
-            }],
-            "currenttime": 1573473597,
-            "reminder": {
-                "reminderWithContent": [],
-                "advertiseContent": []
-            },
-            "request_id": 7501873289383874371
-        })
-    });
-} else {
-    $done({});
-}
+# > 百度云 by ddgksf2013
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/bdcloud.js
+
+hostname = pan.baidu.com
